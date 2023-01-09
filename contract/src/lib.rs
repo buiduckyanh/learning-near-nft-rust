50b16b51b738887ebed81d64d988820d7fa0b9d8
Kian Bui <88682045+buiduckyanh@users.noreply.github.com>
2023-01-09 16:21:13 +0700

    Create Token struct

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,8 +1,11 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{log, near_bindgen, AccountId};
+use near_sdk::{AccountId};
+use near_sdk::serde::Serialize;
 
 
-#[derive(BorshSerialize,BorshDeserialize)]
+#[derive(BorshSerialize,BorshDeserialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+#[derive(Clone)]
 pub struct Token {
     token_id: u128,
     owner_id: AccountId,
