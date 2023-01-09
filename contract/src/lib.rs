b5a4491bdabec2f35ab722d6581c39bec48bfd58
Kian Bui <88682045+buiduckyanh@users.noreply.github.com>
2023-01-09 16:38:35 +0700

    Create NFTContract struct and default()

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,9 +1,9 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{AccountId};
+use near_sdk::collections::UnorderedMap;
 use near_sdk::serde::Serialize;
+use near_sdk::{near_bindgen, AccountId};
 
-
-#[derive(BorshSerialize,BorshDeserialize, Serialize)]
+#[derive(BorshSerialize, BorshDeserialize, Serialize)]
 #[serde(crate = "near_sdk::serde")]
 #[derive(Clone)]
 pub struct Token {
@@ -13,4 +13,22 @@ pub struct Token {
     description: String,
     media_url: String,
     level: u128,
+}
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct NFTContract {
+    owner_by_id: UnorderedMap<u128, AccountId>,
+    token_id: u128,
+    token_by_id: UnorderedMap<u128, Token>,
+}
+
+impl Default for NFTContract {
+    fn default() -> Self {
+        Self {
+            owner_by_id: UnorderedMap::new(b'm'),
+            token_id: 0,
+            token_by_id: UnorderedMap::new(b'n')
+        }
+    }
 }
